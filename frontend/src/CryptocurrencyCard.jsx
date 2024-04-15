6221c13acdcf4f2ab8d09df8c634bcf2a31747a7
mmmurka <murenkodan2005@gmail.com>
2024-04-15 20:42:58 +0300

    conflict resolution

--- frontend/src/CryptocurrencyCard.jsx
+++ frontend/src/CryptocurrencyCard.jsx
@@ -1,9 +1,26 @@
+import {Card} from "antd";
 
 function CryptocurrencyCard() {
 
   return (
     <>
-    hello
+    <Card
+      size="small"
+      title={
+        <div className="flex items-center gap-2">
+          <img src="https://s2.coinmarketcap.com/static/img/coins/64x64/1.png"/>
+          <span>Bitcoin</span>
+        </div>
+      }
+      extra={<a href="#">More</a>}
+      style={{
+        width: 300,
+      }}
+    >
+      <p>Card content</p>
+      <p>Card content</p>
+      <p>Card content</p>
+    </Card>
     </>
   )
 }
