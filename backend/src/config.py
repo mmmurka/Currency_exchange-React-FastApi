d8437b89f6385a911e0e612edde6c5f50d7d90d9
mmmurka <murenkodan2005@gmail.com>
2024-04-15 15:04:03 +0300

    http_client

--- backend/src/config.py
+++ backend/src/config.py
@@ -1,7 +1,10 @@
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
-class Config(BaseSettings):
+class Settings(BaseSettings):
     CMC_API_KEY: str
 
     model_config = SettingsConfigDict(env_file=".env")
+
+
+settings = Settings()
