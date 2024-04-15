ef722b85ea8e0a30dbf54e660083c12a3492a2d8
mmmurka <murenkodan2005@gmail.com>
2024-04-15 15:11:42 +0300

    fixed bugs(.env)

--- backend/src/config.py
+++ backend/src/config.py
@@ -4,7 +4,7 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 class Settings(BaseSettings):
     CMC_API_KEY: str
 
-    model_config = SettingsConfigDict(env_file=".env")
+    model_config = SettingsConfigDict(env_file="backend/.env")
 
 
 settings = Settings()
