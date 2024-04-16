98b0e779a1b33ade0a336f74a2871f41b3d66bd0
mmmurka <murenkodan2005@gmail.com>
2024-04-16 11:16:53 +0300

    updated front

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,8 @@
 import React, {useEffect, useState} from 'react';
 import { AppstoreOutlined, MailOutlined, SettingOutlined } from '@ant-design/icons';
-import { Menu } from 'antd';
-import axios, {Axios} from "axios";
-import CryptocurrencyCard from "./CryptocurrencyCard.jsx";
+import {Menu, Spin} from 'antd';
+import axios from "axios";
+import CryptocurrencyCard from "./components/CryptocurrencyCard.jsx";
 function getItem(label, key, icon, children, type) {
   return {
     key,
@@ -14,54 +14,61 @@ function getItem(label, key, icon, children, type) {
 }
 
 const App = () => {
-
-  const [currencies, setCurrencies] = useState([]);
-  const [currencyId, setCurrencyId] = useState(1);
+  const [currencies, setCurrencies] = useState([])
+  const [currencyId, setCurrencyId] = useState(1)
+  const [currencyData, setCurrencyData] = useState(null)
 
   const fetchCurrencies = () => {
-      axios.get('http://127.0.0.1:8000/cryptocurrencies').then(r  => {
-          const currenciesResponse = r.data
-          const menuItems = [
-              getItem('Список криптовалют 1', 'g1', null, currenciesResponse.map(c => {
-                  return {label: c.name, key: c.id}
-              }),
-                  'group'
-              )
-          ]
-          setCurrencies(menuItems)
-      })
+    axios.get('http://127.0.0.1:8000/cryptocurrencies').then(r => {
+      const currenciesResponse = r.data
+      const menuItems = [
+        getItem('Список криптовалют', 'g1', null,
+          currenciesResponse.map(c => {
+            return {label: c.name, key: c.id}
+          }),
+          'group'
+        )
+      ]
+      setCurrencies(menuItems)
+    })
   }
 
   const fetchCurrency = () => {
-      axios.get(`http://127.0.0.1:8000/cryptocurrencies/${currencyId}`).then(r  => {
-      })
+    axios.get(`http://127.0.0.1:8000/cryptocurrencies/${currencyId}`).then(r => {
+      setCurrencyData(r.data)
+    })
   }
-    useEffect(() => {
-        fetchCurrencies()
-    }, []);
 
-    useEffect(() => {
-        fetchCurrency()
-    }, [currencyId]);
+  useEffect(() => {
+    fetchCurrencies()
+  }, []);
+
+
+  useEffect(() => {
+    setCurrencyData(null)
+    fetchCurrency()
+  }, [currencyId]);
+
   const onClick = (e) => {
     setCurrencyId(e.key)
   };
+
   return (
-    <div className="flex">
-            <Menu
-      onClick={onClick}
-      style={{
-        width: 256,
-      }}
-      defaultSelectedKeys={['1']}
-      defaultOpenKeys={['sub1']}
-      mode="inline"
-      items={currencies}
-      className="h-screen overflow-scroll"
-    />
-        <div className="mx-auto my-auto">
-            <CryptocurrencyCard/>
-        </div>
+    <div className="flex ">
+      <Menu
+        onClick={onClick}
+        style={{
+          width: 256,
+        }}
+        defaultSelectedKeys={['1']}
+        defaultOpenKeys={['sub1']}
+        mode="inline"
+        items={currencies}
+        className="h-screen overflow-scroll"
+      />
+      <div className="mx-auto my-auto">
+        {currencyData ? <CryptocurrencyCard currency={currencyData}/> : <Spin size="large"/>}
+      </div>
     </div>
   );
 };
